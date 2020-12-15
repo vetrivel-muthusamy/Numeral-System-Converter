ea9e219d09d3499cba99840f24dab14d7888ee05
vetrivel-muthusamy <vetrivel.muthusamy@hotmail.com>
2020-12-15 23:18:14 +0530

    Stage 1: completed

--- Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/task/src/converter/Main.java
@@ -2,6 +2,7 @@ package converter;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        //System.out.println("Hello, world!");
+        System.out.println("42 = 0b101010");
     }
 }
